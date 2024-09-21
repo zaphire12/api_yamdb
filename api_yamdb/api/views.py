b94f8984e9c680aa749d3650cb97fdacac60efef
Elvira Galieva <elviragalieva2002@mail.ru>
2024-09-21 11:30:30 +0300

    Добавила filters.py и views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from api.filters import TitleFilter
+from api.permissions import IsAdminOrReadOnly
+from api.serializers import (
+    CategorieSerializer, GenreSerializer, TitleGetSerializer, TitleSerializer
+)
+from reviews.models import Categorie, Genre, Title
+
+
+class CategorieViewSet(mixins.ListModelMixin,
+                       mixins.CreateModelMixin,
+                       mixins.DestroyModelMixin,
+                       viewsets.GenericViewSet):
+    """Получаем/создаем/удаляем категорию."""
+    queryset = Categorie.objects.all()
+    serializer_class = CategorieSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewset(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    """Получаем/создаем/удаляем жанр."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Получаем/создаем/удаляем/редактируем произведение."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleGetSerializer
+        return TitleSerializer
